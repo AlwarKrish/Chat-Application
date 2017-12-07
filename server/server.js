4860db806a3adc905caa0d0ff7b3cdb930ac93a8
Alwar <alwarg1998@gmail.com>
2017-12-07 12:23:58 +0530

    Basic express application

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname,'../public');
+
+// console.log(__dirname+'/../public');
+// console.log(publicPath);
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(3000,() => {
+  console.log(`Server is up on port ${port}`);
+})
